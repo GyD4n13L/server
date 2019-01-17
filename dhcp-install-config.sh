72edee5e6ac8f887540946ea5188aeab2454e1ec
GyD4n13L <32672899+GyD4n13L@users.noreply.github.com>
2019-01-17 16:18:49 +0100

    Update dhcp-install-config.sh

--- dhcp-install-config.sh
+++ dhcp-install-config.sh
@@ -6,8 +6,20 @@ sudo apt-get install isc-dhcp-server -y
 
 ifconfig -a
 
+while true; do
+echo " Which network adapter use DHCP/LAN interfaces?"
 echo "LAN adapter name:"
 
-    read -p " please enter your preference: [eth0|eth1|eth2|eth3|eth4]: " adapter
+    read -p " please enter your preference: [0|1|2|3]: " adapter
     case $adapter in
       [eth0]* ) echo 'INTERFACES="eth0"' >> /etc/default/isc-dhcp-server
+                break;;
+      [eth1]* ) echo 'INTERFACES="eth1"' >> /etc/default/isc-dhcp-server
+                break;;
+      [eth2]* ) echo 'INTERFACES="eth2"' >> /etc/default/isc-dhcp-server
+                break;;
+      [eth3]* ) echo 'INTERFACES="eth3"' >> /etc/default/isc-dhcp-server
+                break;;
+      * ) echo " please answer [1], [2] or [3]";;
+    esac
+done
